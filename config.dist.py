bda92468f597623e5244ceaa24a7c5c7387416d4
belims <bullit88@mail.ru>
2018-12-07 09:10:30 +0300

    Небольшие исправления конфига

--- config.dist.py
+++ config.dist.py
@@ -4,3 +4,9 @@ template_file = 'template.docx'
 name_max_len = 100
 awaiting_time = 3
 log_name = 'info.log'
+
+procedure_number_column = 1
+name_column = 2
+inn_column = 3
+money_column = 9
+address_column = 18
